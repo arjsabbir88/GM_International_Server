1155be976f527a837b54d69fe1f20c5da6b406be
arjsabbir88 <arjsabbir88@gmail.com>
2025-11-12 18:35:15 +0600

    added two api for for user dashboard

--- app.js
+++ app.js
@@ -26,8 +26,11 @@ async function run() {
     await client.connect();
     const db = client.db("GM_International");
     const scholarshipPackage = db.collection("scholarship_package_management");
-    const applicationSubmitCollection = db.collection('application_submit_by_user');
-    const manageUniversity = db.collection('university_management');
+    const applicationSubmitCollection = db.collection(
+      "application_submit_by_user"
+    );
+    const manageUniversity = db.collection("university_management");
+    const usersCollection = db.collection("users");
 
     app.get("/admin/scholarship-package-management", async (req, res) => {
       const data = await scholarshipPackage.find().toArray();
@@ -35,7 +38,36 @@ async function run() {
       res.send(data);
     });
 
+    app.get("users/user-role", async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        return res.send({ role: "user" });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (!user) {
+        return res.send({ role: "user" });
+      }
+      res.send({ role: user.role });
+    });
 
+    app.get("/user/application-status", async (req, res) => {
+      const email = req.query.email;
+      console.log(email)
+      if (!email) {
+        return res.send({ status: "No applications found" });
+      }
+      const query = { email: email };
+      const applications = await applicationSubmitCollection
+        .find(query)
+        .toArray();
+
+        console.log(applications)
+      if (applications.length === 0) {
+        return res.send({ status: "No applications found" });
+      }
+      res.send(applications);
+    });
 
     // get the single data
     app.get("/admin/scholarship-package-management/:id", async (req, res) => {
@@ -66,60 +98,63 @@ async function run() {
 
     // application submit on applicationSubmit collections
 
-    app.post("/student-home-page/student-package-offer/user-application-form", async(req,res) =>{
-      const applicationFormData = req.body;
-      if(!applicationFormData){
-        res.send("Data not found");
+    app.post(
+      "/student-home-page/student-package-offer/user-application-form",
+      async (req, res) => {
+        const applicationFormData = req.body;
+        if (!applicationFormData) {
+          res.send("Data not found");
+        }
+        const result = await applicationSubmitCollection.insertOne(
+          applicationFormData
+        );
+
+        if (!result) {
+          res.send("Something is worng mongodb not connected successfully");
+        }
+
+        res.send(result);
       }
-      const result = await applicationSubmitCollection.insertOne(applicationFormData);
+    );
 
-      if(!result){
-        res.send("Something is worng mongodb not connected successfully")
-      }
-
-      res.send(result);
-    })
-
-    app.post('/mange-university/admin',async(req,res)=>{
+    app.post("/mange-university/admin", async (req, res) => {
       const universityData = req.body;
 
-      if(!universityData){
+      if (!universityData) {
         res.send("Data not found");
       }
       const result = await manageUniversity.insertOne(universityData);
 
-      if(!result){
+      if (!result) {
         res.send("Something is worng!! Database not connected successfully");
       }
 
       res.send(result);
-    })
+    });
 
-
-    app.get('/mange-university/admin', async(req,res)=>{
+    app.get("/mange-university/admin", async (req, res) => {
       const managedUniversityData = await manageUniversity.find().toArray();
-      res.send(managedUniversityData)
-    })
+      res.send(managedUniversityData);
+    });
 
-
-    app.get(`/manage-university/details/:id`, async(req,res)=>{
+    app.get(`/manage-university/details/:id`, async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
 
       const result = await manageUniversity.findOne(query);
 
-      if(!result) return res.send("Data not found");
+      if (!result) return res.send("Data not found");
 
       res.send(result);
-
-    })
-
+    });
 
     // applicatoin collection get api
-    app.get("/admin/manage-application", async(req,res)=>{
-      const allApplicationData = await applicationSubmitCollection.find().toArray();
-      res.send(allApplicationData) 
-    })
+    app.get("/admin/manage-application", async (req, res) => {
+      const allApplicationData = await applicationSubmitCollection
+        .find()
+        .toArray();
+      res.send(allApplicationData);
+    });
 
     console.log("Connected to MongoDB Atlas successfully!");
   } catch (err) {
